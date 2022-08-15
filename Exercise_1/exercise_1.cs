f71cbcea6fa5168fb757a3bb809023530f581a9d
Candlejack <garouspeak@gmail.com>
2022-08-15 16:45:01 +0300

    Exercise_1_is done

---
+++ Exercise_1/exercise_1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+ 
+ 
+ 
+class Palindr
+ {
+    public static bool Palindromtest (string num)
+    {
+        for (int i = 0, j = num.Length - 1; i < j; i++, j--)
+            if (num[i] != num[j])
+            return false;
+            return true;
+ 
+    }
+    static void Main()
+    {   
+    string num;
+    Console.WriteLine("Введите число:");
+    num = Console.ReadLine();
+    if (Palindromtest(num))
+        Console.WriteLine("Это число - палиндром");
+    else Console.WriteLine("Это число - не палиндром");
+    }
+}

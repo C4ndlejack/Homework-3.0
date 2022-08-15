84aae55096128201f51a27d0e144137be24e3500
Candlejack <garouspeak@gmail.com>
2022-08-15 18:31:16 +0300

    Exercise_3_is done

---
+++ Exercise_3/exercise_3.cs
@@ -0,0 +1,14 @@
+﻿using System;
+{
+    Console.WriteLine("Введите число: ");
+    double n = Convert.ToDouble(Console.ReadLine());
+    double i = 1;
+    double expo = 3;
+    Console.WriteLine("Таблица кубов от 1 до введенного числа: ");
+    while (i<=n)
+    {
+        double result = Math.Pow(i, expo);
+        Console.Write(result + " ");
+        i++;
+    }
+}
